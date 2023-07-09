ccea49dd42773b60dd4c5e1a5b514c89bf9279a0
Om Sharma <omsharma@Oms-MacBook-Air.local>
2023-07-09 23:27:33 +0530

    user sign in and sign up portal added

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,3 +1,14 @@
+// const User= require('./model/user');
+
 module.exports.profile=function(req,res){
     res.end("<h1>User profile</h1>");
+}
+
+module.exports.sign_in=function(req,res){
+    res.render("user_sign_in");
+    
+}
+module.exports.sign_up=function(req,res){
+    res.render("user_sign_up");
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,8 @@ const userController= require("../controller/users_controller");
 
 
 router.get('/profile',userController.profile);
+router.get('/sign_in',userController.sign_in);
+router.get('/sign_up',userController.sign_up);
 
 
 
